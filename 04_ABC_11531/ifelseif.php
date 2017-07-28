cffa2c4528e38a9852f8dc0762d9a600da3f6fa7
usmanadina <hmdusmana@gmail.com>
2017-07-28 22:33:21 +0700

    mengedit file ifelseif.php

--- 04_ABC_11531/ifelseif.php
+++ 04_ABC_11531/ifelseif.php
@@ -1,4 +1,14 @@
 <?php
+$x = "B-";
+	if ($x == "A" || $x == "A\B" || $x == "B" || $x == "B-" || $x == "B/C"){
+		echo "LULUS";
+	}elseif($x == "C+" || $x == "C" || $x == "C-" || $x == "C/D" ){
+		echo "LULUS SEBAIKNYA DIULANG";
+	}elseif($x=="D+" || $x=="D"){
+		echo "LULUS & WAJIB DIULANG";
+	}else{
+		echo "TIDAK LULUS";
+	}
 
 
 ?>
